c6869c86a58c65468e9140f3976034edc400719d
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2024-12-13 13:27:45 -0600

    created search bar with icon

--- lib/views/my_app_home_screen.dart
+++ lib/views/my_app_home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:recipe_app/utils/constraints.dart';
+import 'package:recipe_app/widgets/my_icon_button.dart';
 
 class MyAppHomeScreen extends StatefulWidget {
   const MyAppHomeScreen({super.key});
@@ -11,30 +13,69 @@ class MyAppHomeScreen extends StatefulWidget {
 class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       backgroundColor: kbackgroundColor,
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                child: Column(children: [
-                  Row(
-                    children: [
-                      Text(
-                        "What are you\ncooking today?",
-                        style: TextStyle(
-                        fontSize: 32,
-                        fontWeight:
-                        FontWeight.bold,
-                        height: 1),
-                      )
-                  ],)
-                ],),
-              ),
-      ],),),)
+              mySearchBar()
+            ],
+          ),
+        ),
+      )
     );
+  }
+
+  Padding mySearchBar() {
+    return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                  children: [
+                    headerParts(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 22),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            filled: true,
+                            prefixIcon: const Icon(Iconsax.search_normal),
+                            fillColor: Colors.white,
+                            border: InputBorder.none,
+                            hintText: "Search Any Recipes!",
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none
+                          ),
+                          hintStyle: const TextStyle(
+                                color: Colors.grey
+                            ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none
+                          )
+                        ),
+                      ),
+                    ),
+                  ],
+              ),
+            );
+  }
+
+  Row headerParts() {
+    return Row(
+                  children: [
+                    const Text(
+                      "What are you\ncooking today?",
+                      style: TextStyle(
+                      fontSize: 32,
+                      fontWeight:
+                      FontWeight.bold,
+                      height: 1),
+                    ),
+                    const Spacer(),
+                    MyIconButton(icon: Iconsax.notification,
+                        pressed: () {})
+                ],);
   }
 }
